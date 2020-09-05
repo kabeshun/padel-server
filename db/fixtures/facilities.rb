66ede77a2960ae37e8292910b18de98cb9e1648e
kabeshun <shungo12tennis@gmail.com>
2020-09-06 02:03:34 +0900

    add fixtures

---
+++ db/fixtures/facilities.rb
@@ -0,0 +1,7 @@
+Facility.seed(:id,
+                { id: 1, name: 'パデル東京', image_url: 'http://localhost:3000/padeltokyo.jpg' },
+                { id: 2, name: 'パデルワン', image_url: 'http://localhost:3000/padelone.jpg' },
+                { id: 3, name: '所沢フットサルパーク', image_url: 'http://localhost:3000/tokorozawa.jpg' },
+                { id: 4, name: 'パデル＆フットサル晴れのち晴れ', image_url: 'http://localhost:3000/harehare.jpg' },
+                { id: 5, name: 'GIARDINO', image_url: 'http://localhost:3000/giardino.jpg' },
+                { id: 6, name: 'パデル名古屋', image_url: 'http://localhost:3000/padelnagoya.jpg' })
