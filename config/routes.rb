45d2d616b84f8e19192ffa58b5e3091cde0fff08
kabeshun <shungo12tennis@gmail.com>
2020-09-08 01:14:02 +0900

    add admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'admin/home#index'
+  namespace :admin do
+    resources :home, only: %i[index show]
+  end
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :facilities, only: %i[index show]
